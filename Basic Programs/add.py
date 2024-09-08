4a9d31ee1206605cdfe35365873c9ff219e771bc
amirkhan1092 <amirkhan1092@gmail.com>
2024-09-08 16:29:44 +0530

    first addition

---
+++ Basic Programs/add.py
@@ -0,0 +1,8 @@
+# Addition of two number
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("Sum of {0} and {1} is {2}".format(num1, num2, sum))
